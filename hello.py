9dd835dfa45a9011780006ac5856866bcaed14ba
JunLeeDGU <lsjmc0224@dgu.edu>
2022-09-29 15:18:41 +0900

    크래시 해결

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 name = input("이름을 입력하세요: ")
 num = input("학번을 입력하세요: ")
 major = input("학과를 입력하세요: ")
+school = input("학교를 입력하세요: ")
 grade = input("학년을 입력하세요: ")
 
 
@@ -8,4 +9,5 @@ print("<출력>")
 print(f"이름: {name}")
 print(f"학번: {num}")
 print(f"학과: {major}")
+print(f"school: {school}")
 print(f"학년: {grade}")
